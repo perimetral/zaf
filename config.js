c5396fbc4ce183cf2d36af35135f5f0c88f57815
perimetral <themtrx@protonmail.com>
2017-09-12 00:30:39 +0300

    init full

---
+++ config.js
@@ -0,0 +1,27 @@
+const cfg = {};
+
+const tools = require('./lib/tools');
+
+cfg.port = process.env.PORT || 3000;
+cfg.morganMode = 'combined';
+cfg.defaultLayout = 'main';
+
+cfg.paths = {};
+cfg.paths.static = './public';
+cfg.paths.hbsPartials = './views/partials';
+cfg.paths.sessionStore = './db/sessionStore.db';
+
+cfg.session = {};
+cfg.session.secret = tools.randomStringSync(32);
+cfg.session.resave = true;
+cfg.session.saveUninitialized = true;
+cfg.session.cookie = {
+	path: '/',
+	maxAge: 24 * 3600 * 1000,
+};
+cfg.session.unset = 'destroy';
+
+cfg.https = {};
+cfg.https.enabled = false;
+
+module.exports = cfg;
--- index.js
+++ index.js
@@ -1,1 +1,59 @@
+global.$ = {};
+$.cfg = require('./config');
+$.tools = require('./lib/tools');
+
+const express = require('express');
 const nedb = require('nedb');
+const session = require('express-session');
+const socketio = require('socket.io');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const hbs = require('express-handlebars');
+const NedbSessionStore = require('nedb-session-store')(session);
+const protocol = require($.cfg.https.enabled ? 'https' : 'http');
+
+const app = express();
+
+app.use(morgan($.cfg.morganMode));
+app.use(cookieParser);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static($.cfg.paths.static));
+app.use(session({
+	secret: $.cfg.session.secret,
+	resave: $.cfg.session.resave,
+	saveUninitialized: $.cfg.session.saveUninitialized,
+	cookie: $.cfg.session.cookie,
+	store: new NedbSessionStore({
+		filename: $.cfg.paths.sessionStore,
+	}),
+	unset: $.cfg.session.unset,
+}));
+app.engine('hbs', hbs({
+	defaultLayout: $.cfg.defaultLayout,
+}));
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res, next) => {
+	console.log('atata');
+	return res.render('mainp', {
+		title: 'ZAF',
+		test1: 'atata',
+	});
+});
+
+const socketHandler = require('./lib/socketHandler');
+let server = protocol.Server(app);
+let io = socketio(server);
+
+server.listen($.cfg.port, () => {
+	console.log(`LISTENING ON ${$.cfg.port}`);
+});
+
+io.on('connection', (socket) => {
+	socketHandler(socket).then((result) => {
+		if (result) console.log(`SOCKET RESULT: ${result}`);
+	}).catch((e) => {
+		if (e) console.log(`SOCKET ERROR: ${e}`);
+	});
+});
